f02fea0b49339cd51d31a55d324b5245e04c6ebd
Carly <cj.langlois.cl@gmail.com>
2016-08-23 21:24:39 -0500

    first error success

--- main.py
+++ main.py
@@ -1,25 +1,104 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 
+
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Carly's Signup</title>
+    <style type="text/css">
+        .err {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>Signup for Carly</h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
 class MainHandler(webapp2.RequestHandler):
+    """ Handles requests coming in to '/' (the root of our site)
+        e.g. www.signup.com/
+    """
+
     def get(self):
-        self.response.write('Hello world!')
+
+        signup_form = """
+        <form action="/welcome" method="post">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <label for="username">Username</label>
+                        </td>
+                        <td>
+                            <input name="username" type="text">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="password">Password</label>
+                        </td>
+                        <td>
+                            <input name="password" type="password">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="verify">Verify Password</label>
+                        </td>
+                        <td>
+                            <input name="verify" type="password">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="email">Email (optional)</label>
+                        </td>
+                        <td>
+                            <input name="email" type="email">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <input type="submit">
+        </form>
+        """
+        # if we have an error, make a <p> to display it
+        err = self.request.get("err")
+        err_element = "<p class= 'err'>" + err + "</p>" if err else ""
+
+        # combine all the pieces to build the content of our signup page
+        signup_content = page_header + signup_form + err_element + page_footer
+        self.response.write(signup_content)
+
+class Welcome (webapp2.RequestHandler):
+    """ Handles requests coming in to '/welcome' (after successful signup)
+        e.g. www.signup.com/welcome
+    """
+
+    def post(self):
+        # look inside the request to figure out what the user typed
+        username = self.request.get("username")
+
+        if username == "":
+            no_name = "Please specify a username."
+            self.redirect('/?err={}'.format(no_name))
+
+        #build response content
+        welcome_message = "Welcome, " + username + "!"
+        welcome_content = page_header + welcome_message + page_footer
+        self.response.write(welcome_content)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', Welcome)
 ], debug=True)
