c0ac4efd85448041e915d9cdfa825eb9fed6c6f2
Carly <cj.langlois.cl@gmail.com>
2016-08-24 22:39:19 -0500

    username complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import webapp2
+import re
 
 
 # html boilerplate for the top of every page
@@ -23,14 +24,23 @@ page_footer = """
 </html>
 """
 
+# define a valid username
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+
 class MainHandler(webapp2.RequestHandler):
     """ Handles requests coming in to '/' (the root of our site)
         e.g. www.signup.com/
     """
 
     def get(self):
+        # if we have an error, make a <p> to display it
+        err = self.request.get("err")
+        err_element = "<p class='err'>" + err + "</p>" if err else ""
 
-        signup_form = """
+        username_form = """
         <form action="/welcome" method="post">
             <table>
                 <tbody>
@@ -41,43 +51,36 @@ class MainHandler(webapp2.RequestHandler):
                         <td>
                             <input name="username" type="text">
                         </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="password">Password</label>
-                        </td>
-                        <td>
-                            <input name="password" type="password">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="verify">Verify Password</label>
-                        </td>
-                        <td>
-                            <input name="verify" type="password">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email">Email (optional)</label>
-                        </td>
-                        <td>
-                            <input name="email" type="email">
-                        </td>
+                        <td class="err">{}</td>
                     </tr>
                 </tbody>
             </table>
             <input type="submit">
-        </form>
-        """
-        # if we have an error, make a <p> to display it
-        err = self.request.get("err")
-        err_element = "<p class= 'err'>" + err + "</p>" if err else ""
+        </form>""".format(err_element)
+
 
         # combine all the pieces to build the content of our signup page
-        signup_content = page_header + signup_form + err_element + page_footer
+        signup_form = username_form
+
+        signup_content = page_header + signup_form + page_footer
         self.response.write(signup_content)
+
+
+    def post(self):
+        # look inside the request to figure out what the user typed
+        username = self.request.get("username")
+
+
+
+        # if the user's username is not valid
+        # TODO - invalid username
+
+        # if the user's password and password-confirmation do not match
+        # TODO - passwords dont match
+
+        # if the user provides an email, but it's not a valid email.
+        # TODO - invalid email
+
 
 class Welcome (webapp2.RequestHandler):
     """ Handles requests coming in to '/welcome' (after successful signup)
@@ -85,12 +88,12 @@ class Welcome (webapp2.RequestHandler):
     """
 
     def post(self):
-        # look inside the request to figure out what the user typed
+
         username = self.request.get("username")
 
-        if username == "":
-            no_name = "Please specify a username."
-            self.redirect('/?err={}'.format(no_name))
+        if not valid_username(username):
+            name_err = "Please enter a valid username"
+            self.redirect('/?err={}'.format(name_err))
 
         #build response content
         welcome_message = "Welcome, " + username + "!"
